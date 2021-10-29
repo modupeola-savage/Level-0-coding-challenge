c501b19fe4411a376ea4f8e926646775205dee01
Modupeola Savage <92022801+modupeola-savage@users.noreply.github.com>
2021-10-29 11:13:13 +0100

    Corrected Task 0.10

--- task1.0.py
+++ task1.0.py
@@ -1,9 +1,9 @@
 def common_letters(first, second):
-    common = ["Common letters:"]
+    common = "Common letters: "
     for i in set(first):
         for j in set(second):
             if i == j:
-                common.append(i)
+                common += i + ","
     return common
 
 
